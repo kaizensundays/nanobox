62951b4fc0929b4efd413f08af2941e473e9acca
KaizenSundays <kaizensundays@localhost>
2024-03-23 20:11:32 -0400

    [ nano-box ] :service

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -3,7 +3,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm")
     id("org.springframework.boot") version "2.7.18"
+    id("com.jfrog.artifactory") version "4.29.0"
+    id("module.publication")
 }
+
+group = "com.kaizensundays.fusion"
+version = "0.0.1-SNAPSHOT"
 
 dependencies {
 
@@ -27,4 +32,42 @@ tasks.withType<Test> {
 tasks.bootJar {
     archiveFileName.set("service.jar")
     destinationDirectory.set(file("bin"))
+}
+
+tasks.publish {
+    dependsOn("assemble")
+}
+
+configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>("bootJava") {
+            from(components["java"])
+            artifact(tasks.bootJar) {
+                artifactId = "nano-box"
+            }
+        }
+    }
+}
+
+publishing {
+    repositories {
+        mavenLocal()
+    }
+}
+
+artifactory {
+    setContextUrl(project.properties["artifactory.url"] as String)
+    publish {
+        repository {
+            setRepoKey("libs-snapshot-local")
+            setUsername(project.properties["artifactory.username"] as String)
+            setPassword(project.properties["artifactory.password"] as String)
+            setMavenCompatible(true)
+        }
+        defaults {
+            publications("bootJava")
+            setPublishArtifacts(true)
+            setPublishPom(true)
+        }
+    }
 }
