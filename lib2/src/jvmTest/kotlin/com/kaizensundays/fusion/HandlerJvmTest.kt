633cd0c74ecc5557e49f8304d6696f2a9d8e6a05
KaizenSundays <kaizensundays@localhost>
2024-03-30 19:10:53 -0400

    [ lib2 ] loadProperties

--- lib2/src/jvmTest/kotlin/com/kaizensundays/fusion/HandlerJvmTest.kt
+++ lib2/src/jvmTest/kotlin/com/kaizensundays/fusion/HandlerJvmTest.kt
@@ -1,5 +1,7 @@
 package com.kaizensundays.fusion
 
+import okio.FileSystem
+import okio.Path.Companion.toPath
 import org.junit.jupiter.api.Test
 
 /**
@@ -12,8 +14,20 @@ class HandlerJvmTest {
     private val handler = Handler()
 
     @Test
-    fun test() {
+    fun handle() {
         handler.handle("Ok")
+    }
+
+    @Test
+    fun loadProperties() {
+
+        val fs = FileSystem.SYSTEM
+
+        val path = fs.canonicalize("src/jvmTest/resources/test.properties".toPath())
+        println("path=$path")
+
+        val s = fs.read(path) { readUtf8() }
+        println(s)
     }
 
 }
