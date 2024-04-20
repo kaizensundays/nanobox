a5f3ef5a6211cc176bd6f7d1870be345c2c73968
KaizenSundays <kaizensundays@localhost>
2024-04-20 15:18:47 -0400

    [ nanobox ] - :library

--- library/src/commonMain/kotlin/Calculator.kt
+++
@@ -1,9 +0,0 @@
-class Calculator {
-
-    fun calculate(): Double {
-        return 0.37
-    }
-
-}
-
-expect fun <C : CalculationContext> C.calculateOption(): Double
--- library/src/jvmMain/kotlin/fibiprops.jvm.kt
+++
@@ -1,6 +0,0 @@
-actual val firstElement: Int = 2
-actual val secondElement: Int = 3
-
-actual fun <C : CalculationContext> C.calculateOption(): Double {
-    return Calculator().calculate()
-}
