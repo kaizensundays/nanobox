b29d37fe1e3a1c4dd9edcea0210dceafe9db01b1
KaizenSundays <kaizensundays@localhost>
2024-12-21 12:47:39 -0500

    [ nanobox ] Spring Boot 3.4.1

--- buildSrc/src/main/kotlin/nanobox-versions.kt
+++ buildSrc/src/main/kotlin/nanobox-versions.kt
@@ -5,7 +5,7 @@
  */
 
 const val KotlinVersion = "2.0.21"
-const val SpringBootVersion = "2.7.18"
+const val SpringBootVersion = "3.4.1"
 const val JFrogArtifactoryPluginVersion = "5.2.5"
 
 const val bundleName = "nanobox"
